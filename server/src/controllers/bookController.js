8d02533efb5c6c18cf357cd1b1e618c2a3f6fb8b
Antti Pikkuaho <antti.pikkuaho@outlook.com>
2020-11-11 15:08:18 +0200

    getBook and some eslint fixes

--- server/src/controllers/bookController.js
+++ server/src/controllers/bookController.js
@@ -2,9 +2,16 @@ import { json } from "express";
 import bookModel from "../models/bookModel.js";
 
 export const newBook = async (req, res) => {
-    const { isbn, title, author, published, pages, description } = req.body;
+    const {
+        isbn,
+        title,
+        author,
+        published,
+        pages,
+        description,
+    } = req.body;
     const Book = bookModel;
-    const book = new Book ({
+    const book = new Book({
         isbn,
         title,
         author,
@@ -22,4 +29,12 @@ export const newBook = async (req, res) => {
     });
     await book.save();
     res.json(book);
-}
+};
+
+export const getBook = async (req, res) => {
+    const book = await bookModel.findOne({ isbn: req.body.isbn });
+    if (!book) {
+        return res.status(404).send(`No book found by ISBN ${req.body.isbn}`);
+    }
+    return res.json(book);
+};
