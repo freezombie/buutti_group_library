e546ce8774e3805fe59526e0cd28cda3813b9779
FreeZombie <antti.pikkuaho@outlook.com>
2020-11-12 13:45:52 +0200

    Start of booksearch

--- server/src/controllers/bookController.js
+++ server/src/controllers/bookController.js
@@ -23,3 +23,13 @@ export const newBook = async (req, res) => {
     await book.save();
     res.json(book);
 }
+
+export const searchBook = async (req, res) => {
+    if("title" in req.body) {
+            const matches = await bookModel.find({ title: { $regex: req.body.title, $options: "i" } });
+            if(Object.entries(matches).length === 0) {
+                return res.status(404).send("No matches found");
+            }
+            return res.json(matches);
+    }
+}
--- server/src/routes/bookRouter.js
+++ server/src/routes/bookRouter.js
@@ -2,11 +2,12 @@ import express from "express";
 import {
     //function names
     newBook,
+    searchBook,
 } from "../controllers/bookController.js";
 const router = express.Router();
 
 //router.get("/", getBook);
-//router.get("/search", searchBook);
+router.get("/search", searchBook);
 //router.post("/reserve", reserveBook);
 router.post("/", newBook);
 //router.put("/", modifyBook);
