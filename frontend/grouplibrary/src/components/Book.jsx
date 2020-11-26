bafc0ddb24426b8bf57488842abaf95135b8d1a6
FreeZombie <antti.pikkuaho@outlook.com>
2020-11-26 14:16:43 +0200

    Copies visible

--- frontend/grouplibrary/src/components/Book.jsx
+++ frontend/grouplibrary/src/components/Book.jsx
@@ -1,11 +1,12 @@
 import { Tiles } from "@rebass/layout";
+import Copy from "./Copy";
 
 function Book (props) {
-    console.log(props.data);
     const { isbn, title, author, img, published, description} = props.data;
     const year = published.slice(0, 4);
     const month = published.slice(6,7);
     const day = published.slice(9,10);
+    
     return(
         <Tiles columns={1, null , 2}>
             <img src={img} />
@@ -15,6 +16,11 @@ function Book (props) {
                 <h3>{day}.{month}.{year}</h3>
                 <p><b>ISBN:</b> {isbn}</p>
                 <p><b>Description:</b> {description}</p>
+                {props.data.copies.map((copy) => {
+                    return(
+                        <Copy data={copy} />
+                    )
+                })}
             </div>
         </Tiles>
     );
