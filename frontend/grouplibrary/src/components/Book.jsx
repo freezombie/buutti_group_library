d2279e2ef5d7821d889f25495c19da63bfe8f718
FreeZombie <antti.pikkuaho@outlook.com>
2020-11-27 11:53:29 +0200

    Copies shown as numbers instead of elements.

--- frontend/grouplibrary/src/components/Book.jsx
+++ frontend/grouplibrary/src/components/Book.jsx
@@ -1,13 +1,17 @@
+import { Flex } from "rebass";
 import { Tiles } from "@rebass/layout";
-import Copy from "./Copy";
+import { Button } from "rebass";
+import "./book.css"
 
 function Book (props) {
     const { isbn, title, author, img, published, description} = props.data;
     const year = published.slice(0, 4);
     const month = published.slice(6,7);
     const day = published.slice(9,10);
-    
+    const availableCopies = props.data.copies.filter(copy => copy.status==="in_library");
+
     return(
+    <div className="book">
         <Tiles columns={1, null , 2}>
             <img src={img} />
             <div>
@@ -16,13 +20,14 @@ function Book (props) {
                 <h3>{day}.{month}.{year}</h3>
                 <p><b>ISBN:</b> {isbn}</p>
                 <p><b>Description:</b> {description}</p>
-                {props.data.copies.map((copy) => {
-                    return(
-                        <Copy data={copy} />
-                    )
-                })}
             </div>
         </Tiles>
+        <Flex className="reserveArea">
+            <p><b>Total copies: </b> {props.data.copies.length}</p>
+            <p><b>Copies available:</b> {availableCopies.length}</p>
+            <Button>{availableCopies != 0 ? "Loan" : "Reserve"}</Button>
+        </Flex>
+    </div>
     );
 }
 
